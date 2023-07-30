567ea32b1ebaf4f839c6ecce58c6ffa087ea0099
HeshamJean <heshamjean13@gmail.com>
2023-07-30 10:56:46 +0300

    fixed with routings

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
 
-app = Flask(__name__, template_folder='templates', static_folder='static')
-app.config['SECRET_KEY'] = 'super-secret-key'
+app = Flask(__name__, template_folder='templets', static_folder='static')
+app.config['SECRET_KEY'] = 'super-secret-key '
 
 #Code goes below here
 
-
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 
 #Code goes above here
