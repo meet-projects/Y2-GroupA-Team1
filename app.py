dfe88fd017f32ed3e9aa2fba870775a013ebb02f
juliaka-meet <juliakagan109@gmail.com>
2023-07-31 17:05:33 +0300

    with about page

--- app.py
+++ app.py
@@ -11,8 +11,9 @@ app.config['SECRET_KEY'] = 'super-secret-key '
 def home():
     return render_template('index.html')
 
-
-#Code goes above here
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
