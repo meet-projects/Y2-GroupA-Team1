fcfebfe99fe3d9839def0f19f28666a924e9a11d
juliaka-meet <juliakagan109@gmail.com>
2023-08-01 11:59:00 +0300

    legitmatepush

--- app.py
+++ app.py
@@ -5,33 +5,12 @@ import pyrebase
 app = Flask(__name__, template_folder='templets', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key '
 
-firebaseConfig = {
-  "apiKey": "AIzaSyAMng7Xrimcf8DdC-oeSD1iKhHa2aFw5nM",
-  "authDomain": "questions-and-answers-145b9.firebaseapp.com",
-  "projectId": "questions-and-answers-145b9",
-  "storageBucket": "questions-and-answers-145b9.appspot.com",
-  "messagingSenderId": "1041152034637",
-  "appId": "1:1041152034637:web:c4d0b370d55a9dbd3f1904",
-  "measurementId": "G-6J5HH8BP8Y",
-  "databaseURL":'https://questions-and-answers-145b9-default-rtdb.europe-west1.firebasedatabase.app/'
-}
-firebase = pyrebase.initialize_app(firebaseConfig)
-auth=firebase.auth()
-db =firebase.database()
+
 #Code goes below here 
 
 @app.route('/')
 def home():
-    return render_template('index.html')
-@app.route('/Q&A',methods=['GET','POST'])
-def questionandanswer():
-    if request.method=='POST':
-        question = request.form["question"]
-        # Save the question to Firebase Firestore
-        db.collection("qna").add({"question": question, "answer": ""})
-        return redirect("/qna")
-    else:
-        return render_template('Q&A.html')
+    return render_template('new_index.html')
 
 #Code goes above here
 
@@ -40,4 +19,4 @@ def apply():
     return render_template('apply.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5002)
+    app.run(debug=True, port=5003)
